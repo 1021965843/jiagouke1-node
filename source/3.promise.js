ef448101e3d2dc09721dab9be46e071fe285a0b8
Your Name <you@example.com>
2021-03-29 11:04:27 +0800

    'promise-all'

--- source/3.promise.js
+++ source/3.promise.js
@@ -142,6 +142,29 @@ class Promise {
     catch(errorFn){
         return this.then(null,errorFn)
     }
+
+    static all = function(promises) {
+        return new Promise((resolve, reject) => {
+            let result = [];
+            let times = 0;
+            const processSuccess = (index,val)=>{
+                result[index] = val;
+                if(++times === promises.length){
+                    resolve(result)
+                }
+            }
+            for (let i = 0; i < promises.length; i++) { // 并发 多个请求一起执行的
+                let p = promises[i]
+                if(p && typeof p.then === 'function'){
+                    p.then((data)=>{
+                        processSuccess(i,data)
+                    },reject); // 如果其中某一个promise失败了 直接执行失败即可
+                }else{
+                    processSuccess(i,p)
+                }
+            }
+        });
+    }
 }
 // npm install promises-aplus-tests -g
 
