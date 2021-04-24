f3dc8e55bf174b4d1a7852f2972044fd66ceef30
Your Name <you@example.com>
2021-04-24 10:26:36 +0800

    'upload'

--- 14.http-header/1.server.js
+++ 14.http-header/1.server.js
@@ -5,21 +5,24 @@ const fs = require('fs');
 const mime = require('mime');
 const querystring = require('querystring'); // json.parse  json.stringify
 
-Buffer.prototype.split = function (sep) {
+const uuid = require('uuid'); // 第三方模块
+Buffer.prototype.split = function(sep) {
     let len = Buffer.from(sep).length; // 强制将字符串转化成buffer
     let offset = 0;
     let current;
-    
+
     let arr = [];
-    while (-1 !== (current = this.indexOf(sep,offset))) {
-        arr.push( this.slice(offset,current))
+    while (-1 !== (current = this.indexOf(sep, offset))) {
+        arr.push(this.slice(offset, current))
         offset = current + len
     }
+    arr.push(this.slice(offset));
 
-    arr.push()
-   
-    
+    return arr;
+
 }
+
+const uploadPath = path.resolve(__dirname,'upload');
 
 const server = http.createServer((req, res) => {
     let { pathname, query } = url.parse(req.url, true);
@@ -61,18 +64,42 @@ const server = http.createServer((req, res) => {
             // http1.0中的特点 为了能识别不同类型 需要通过请求头来处理
             if (req.headers['content-type'] === 'application/json') {
                 let obj = JSON.parse(buf.toString()); // 回显json
-                res.setHeader('Content-Type','application/json')
+                res.setHeader('Content-Type', 'application/json')
                 res.end(JSON.stringify(obj));
             } else if (req.headers['content-type'] === 'text/plain') {
-                res.setHeader('Content-Type','text/plain')
+                res.setHeader('Content-Type', 'text/plain')
                 res.end(buf.toString());
-            } else if(req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+            } else if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
                 // {username:"123"}  username=123&password=456  a=1; b=2
-                let r = querystring.parse(buf.toString(),'&','='); // 可以将查询字符串 转化成对象
+                let r = querystring.parse(buf.toString(), '&', '='); // 可以将查询字符串 转化成对象
                 res.end(JSON.stringify(r));
-            }else if(req.headers['content-type'].includes('multipart/form-data')){
-                let boundary ="--" +  req.headers['content-type'].split('=')[1];
-                buf.split(boundary)
+            } else if (req.headers['content-type'].includes('multipart/form-data')) {
+                let boundary = "--" + req.headers['content-type'].split('=')[1];
+                let lines = buf.split(boundary).slice(1, -1);
+                const r = {};
+                lines.forEach(line => {
+                    // 把header 和 内容进行拆分
+                    let [head, body] = line.split(`\r\n\r\n`);
+                    head = head.toString();
+                    let name = head.match(/name="(.+?)"/)[1];
+                    if (head.includes('filename')) {
+                        // 文件上传  将文件内容上传到服务器的上传文件夹中
+                        let buffer = line.slice(head.length + 4, -2);
+                        let filename = uuid.v4();
+                        // 你最终创建完名字之后 还会同步到数据库里 ，下次查找数据库，再找到对应的文件名
+                        fs.writeFileSync(path.join(uploadPath,filename),buffer)
+                        r[name] = {
+                            filename,
+                            size:buffer.length
+                            // 文件大小 fs.stat
+                        }
+                        // 文件名一般都是随机的
+                    } else { // key:value
+                        // 直接把信息放到一个对象中即可
+                        r[name] = body.toString().slice(0, -2);
+                    }
+                })
+                res.end(JSON.stringify(r));
             }
         })
     } else {
